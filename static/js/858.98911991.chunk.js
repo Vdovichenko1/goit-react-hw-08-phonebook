66314a628f1f67f8e8dc81436040bfec782152fe
Vdovichenko1 <106983322+Vdovichenko1@users.noreply.github.com>
2022-11-17 16:49:01 +0000

    Deploying to gh-pages from @ Vdovichenko1/goit-react-hw-08-phonebook@d9d242b2bbc76a513fd5f52b0bf07629f4719b38 🚀

---
+++ static/js/858.98911991.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[858],{3858:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var i=n(184),c={container:{minHeight:"calc(100vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function o(){return(0,i.jsxs)("div",{style:c.container,children:[(0,i.jsx)("h1",{style:c.title,children:"Hello, friend!"})," "]})}}}]);
+//# sourceMappingURL=858.98911991.chunk.js.map
