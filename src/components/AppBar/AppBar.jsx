00b55db7c37b42ec6bb02ee70cb13d6722c7bc5e
Anton Vdovichenko <106983322+Vdovichenko1@users.noreply.github.com>
2022-11-18 09:09:26 +0200

    style

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,13 +2,25 @@ import { Box, Text } from '@chakra-ui/react';
 import Navigator from 'components/Navigator';
 import UserMenu from 'components/UserMenu';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { selectLogIn } from 'redux/auth/authSelectors';
 
 export default function AppBar() {
   const logIn = useSelector(selectLogIn);
   return (
-    <Box display = " flex " alignItems = " center " justifyContent = " space-between " marginBottom='50px' marginTop='30px' borderBottom='2px solid green' paddingBottom='7px'>
-      <Text fontSize='22px' fontWeight='700' color='green' marginLeft='10px'> Phonebook </Text>
+    <Box
+      display=" flex "
+      alignItems=" center "
+      justifyContent=" space-between "
+      marginBottom="50px"
+      marginTop="30px"
+      borderBottom="2px solid green"
+      paddingBottom="7px"
+    >
+      <Text fontSize="22px" fontWeight="700" color="green" marginLeft="10px">
+        
+        <Link to='/'>Phonebook</Link>
+      </Text>
       {logIn ? <UserMenu /> : <Navigator />}
     </Box>
   );
--- src/redux/auth/pages/Home.jsx
+++ src/redux/auth/pages/Home.jsx
@@ -1,12 +1,12 @@
-import { Box, Text} from "@chakra-ui/react"
+import { Box, Text } from '@chakra-ui/react';
 
 export default function Home() {
-    return (
-        <Box display="flex"
-    alignItems="center"
-    justifyContent="center">
-            <Text fontWeight={700}
-    fontSize={48}
-    textAlign='center' color='#57c3d5'>Hello, friend! </Text></Box>
-    )
+  return (
+    <Box display="flex-wrap" alignItems="center" justifyContent="center" textAlign='center'>
+      <Text fontWeight={700} fontSize={48} textAlign="center" color="#57c3d5">
+        Hello, friend!{' '}
+          </Text>
+          <Text marginTop='20px' fontWeight={500} fontSize={46} color="green">Come in don't be afraid, come out don't cry!</Text>
+    </Box>
+  );
 }
